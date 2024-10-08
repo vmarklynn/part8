0be4906f6b0e8d9f52b4296f0871ce95b1a59450
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-10-08 00:04:31 -0700

    Fixed queries, added query connection for books

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -6,6 +6,18 @@ export const ALL_AUTHORS = gql`
       name
       born
       bookCount
+      id
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query allBooks($author: String, $genre: String) {
+    allBooks(author: $author, genre: $genre) {
+      title
+      published
+      author
+      id
     }
   }
 `;
